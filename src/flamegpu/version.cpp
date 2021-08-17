9d7422051eda553d2e22df5c3fe8aa232afdf982
Peter Heywood <peethwd@gmail.com>
2021-08-17 23:43:01 +0100

    REVERT THIS COMMIT. Create nerfed flamegpu for fast CI debugging

---
+++ src/flamegpu/version.cpp
@@ -0,0 +1,7 @@
+#include "flamegpu/version.h"
+
+namespace flamegpu {
+
+const char VERSION_EMBEDDED[] = {"todo"};
+
+}  // namespace flamegpu
--- tests/helpers/main.cu
+++ tests/helpers/main.cu
@@ -1,16 +1,16 @@
 #include <cuda_runtime.h>
 #include <cstdio>
 
-#include "flamegpu/gpu/CUDASimulation.h"
+// #include "flamegpu/gpu/CUDASimulation.h"
 #include "gtest/gtest.h"
-#include "helpers/device_initialisation.h"
+// #include "helpers/device_initialisation.h"
 
 
 GTEST_API_ int main(int argc, char **argv) {
   // Disable auto reset, as it slows down test execution quite a bit.
-  flamegpu::CUDASimulation::AUTO_CUDA_DEVICE_RESET = false;
+  // flamegpu::CUDASimulation::AUTO_CUDA_DEVICE_RESET = false;
   // Time the cuda agent model initialisation, to check it creates the context.
-  flamegpu::tests::timeCUDASimulationContextCreationTest();
+  // flamegpu::tests::timeCUDASimulationContextCreationTest();
   // Run the main google test body
   printf("Running main() from %s\n", __FILE__);
   testing::InitGoogleTest(&argc, argv);
